fb519bbc9d1b49dc6de34c7e21fac93cdf5350d6
Rain <raindrops.bsb@gmail.com>
2024-09-24 23:45:41 -0500

    Agregando dependencias y inicializando controlador main

---
+++ src/main/java/org/raindrops/HomeControl.java
@@ -0,0 +1,13 @@
+package org.raindrops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControl {
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+}
