04d902bb18165137da7b1eed07f6c96cc5559ade
Rain <raindrops.bsb@gmail.com>
2024-09-25 00:01:26 -0500

    Repositorio usuario agregado

---
+++ src/main/java/org/raindrops/usuario/Usuario.java
@@ -0,0 +1,60 @@
+package org.raindrops.usuario;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "Usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false,length = 20)
+    private String usuario;
+    @Column(nullable = false,length = 20)
+    private String contrasenia;
+    @Column(nullable = false,unique = true,length = 50)
+    private String email;
+    @Column(nullable = false,length = 2)
+    private String edad;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+}
---
+++ src/main/java/org/raindrops/usuario/UsuarioRepositorio.java
@@ -0,0 +1,6 @@
+package org.raindrops.usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepositorio extends CrudRepository<Usuario,Integer> {
+}
